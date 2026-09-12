c8d7f21a5c672f56ae96911a7e439a05d5c586b2
Bob-phoenix <123460934+Bob-phoenix@users.noreply.github.com>
2026-09-12 15:59:18 +0530

    up

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Missing MONGODB_URI in .env.local");
+  throw new Error("Missing MONGODB_URI environment variable");
 }
 
 let cached = (global as any).mongoose;
